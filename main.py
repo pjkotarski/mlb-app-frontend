1acc75a61cacbdf46df6b0315000c1b73de54a37
Pierce Kotarski <pierejkotarski@utexas.edu>
2019-04-26 23:53:51 -0500

    API working success

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import subprocess
 import simplejson
 from flask import jsonify
 from models import app
+import json
+from mlb import msf, get_stat
 from flask import abort
 from ohmysportsfeedspy import MySportsFeeds
 
@@ -11,15 +13,32 @@ from ohmysportsfeedspy import MySportsFeeds
 
 
 
-@app.route("/ballers/api/")
-def index():
-    msf = MySportsFeeds(version="2.0", verbose = True)
-    msf.authenticate("ea5ffd04-787e-4b3b-a516-0ead12", 'MYSPORTSFEEDS')
-    stats = msf.msf_get_data(league='mlb',season='current',feed='daily_games',format='json', date = '20190426',force = True)
-    print(type(stats))
-    return render_template(stats)
+@app.route("/ballers/api/scores/<date>")
+def scores(date):
 
+    stats = msf.msf_get_data(league='mlb',season='current',feed='daily_games',format='json', date = date,force = True)
 
+    #will open and write to the temporary json file.
+    with open('templates/temp.json', 'w') as t:
+        json.dump(stats['games'], t)
+
+    return render_template('temp.json')
+
+@app.route("/ballers/api/stats")
+def stats():
+    stats = msf.msf_get_data(league = 'mlb', feed = 'seasonal_player_stats',season = 'current', format = 'json')
+
+    types = ['homeruns', 'battingAvg', 'runsBattedIn', 'wins', 'earnedRunAvg', 'pitcherStrikeouts']
+    # 'wins', 'earnedRunAvg', 'pitcherStrikeouts'
+    allStat = []
+
+    for each in types:
+        allStat.append(get_stat(stats['playerStatsTotals'],each))
+
+    with open('templates/temp.json', 'w') as t:
+        json.dump(allStat, t)
+
+    return render_template('temp.json')
 
 
 if __name__ == '__main__':
--- mlb.py
+++ mlb.py
@@ -2,5 +2,47 @@ from ohmysportsfeedspy import MySportsFeeds
 import simplejson
 
 msf = MySportsFeeds(version="2.0", verbose = True)
-print(msf.authenticate("0c83cfaa-63a0-4cc2-9300-3ab9dd", 'MYSPORTSFEEDS'))
-print('at least we get this far')
+msf.authenticate("ea5ffd04-787e-4b3b-a516-0ead12", 'MYSPORTSFEEDS')
+
+def get_stat(all, stat):
+
+    top = []
+
+    if stat in ['homeruns', 'runsBattedIn', 'battingAvg']:
+        mod = 'batting'
+    else:
+        mod = 'pitching'
+
+    for eachPlayer in all:
+        try:
+            eachPlayer['stats'][mod]
+            print(stat)
+            print(mod)
+
+            if len(top) < 5:
+
+                top.append([eachPlayer['player']['firstName'], eachPlayer['player']['lastName'], eachPlayer['stats'][mod][stat]])
+
+
+            else:
+
+                for place in range(len(top)):
+
+                    repIn = float(top[place][2])
+                    repNew = float(eachPlayer['stats'][mod][stat])
+                    if stat == 'earnedRunAvg':
+                        h = repNew
+                        repNew = repIn
+                        repIn = h
+
+                        if repNew >= repIn:
+
+                            player = [eachPlayer['player']['firstName'], eachPlayer['player']['lastName'], eachPlayer['stats'][mod][stat]]
+                            top.insert(place, player)
+                            del top[-1]
+                            break
+
+        except:
+            print('nothing found')
+
+    return top
