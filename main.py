764af29eaa14afb9cc832f909027c854e949aae4
Pierce Kotarski <pierejkotarski@utexas.edu>
2019-04-28 20:12:48 -0500

    API Functions completely working. Still require DB work.

--- main.py
+++ main.py
@@ -4,23 +4,20 @@ import simplejson
 from flask import jsonify
 from models import app
 import json
-from mlb import msf, get_stat
+from mlb import msf, get_stat, get_scores, get_standings
 from flask import abort
 from ohmysportsfeedspy import MySportsFeeds
-
-
-
-
 
 
 @app.route("/ballers/api/scores/<date>")
 def scores(date):
 
-    stats = msf.msf_get_data(league='mlb',season='current',feed='daily_games',format='json', date = date,force = True)
+    daily_scores = msf.msf_get_data(league='mlb',season='current',feed='daily_games',format='json', date = date,force = True)
+    cleaned_scores = get_scores(daily_scores['games'])
 
-    #will open and write to the temporary json file.
+
     with open('templates/temp.json', 'w') as t:
-        json.dump(stats['games'], t)
+        json.dump(cleaned_scores, t)
 
     return render_template('temp.json')
 
@@ -33,10 +30,22 @@ def stats():
     allStat = []
 
     for each in types:
-        allStat.append(get_stat(stats['playerStatsTotals'],each))
+        val = {each : get_stat(stats['playerStatsTotals'],each) }
+        allStat.append(val)
 
     with open('templates/temp.json', 'w') as t:
         json.dump(allStat, t)
+
+    return render_template('temp.json')
+
+@app.route("/ballers/api/standings")
+def standings():
+    standings = msf.msf_get_data(league = 'mlb', feed = 'seasonal_standings', season = 'current', format = 'json', stats = None)
+
+    standings = get_standings(standings['teams'])
+
+    with open('templates/temp.json', 'w') as t:
+        json.dump(standings, t)
 
     return render_template('temp.json')
 
--- mlb.py
+++ mlb.py
@@ -4,6 +4,80 @@ import simplejson
 msf = MySportsFeeds(version="2.0", verbose = True)
 msf.authenticate("ea5ffd04-787e-4b3b-a516-0ead12", 'MYSPORTSFEEDS')
 
+
+
+def get_standings(standings):
+
+    #dictionary of arrays: arrays contain the team name, wins, losses, pct g
+    std = {}
+    for team in standings:
+
+        conference = team['conferenceRank']['conferenceName']
+        division = team['divisionRank']['divisionName']
+
+        division = conference + ' ' + division
+
+        if division not in std.keys():
+            std[division] = [{}]*5
+
+        rank = int(team['divisionRank']['rank'])
+
+        info = {}
+        info['team'] = team['team']['abbreviation']
+        info['rank'] = rank
+        info['wins'] = team['stats']['standings']['wins']
+        info['loses'] = team['stats']['standings']['losses']
+        info['winPct'] = team['stats']['standings']['winPct']
+        info['gamesBack'] = team['divisionRank']['gamesBack']
+
+
+
+        std[division][rank-1] = info
+
+    return std
+
+
+def get_scores(data):
+
+    games = []
+
+    for eachGame in data:
+
+        thisGame = {}
+        status = eachGame['schedule']['playedStatus']
+
+        thisGame['status'] = status
+        thisGame['homeTeam'] = eachGame['schedule']['homeTeam']['abbreviation']
+        thisGame['awayTeam'] = eachGame['schedule']['awayTeam']['abbreviation']
+
+
+        if status == 'UNPLAYED':
+            thisGame['startTime'] = eachGame['schedule']['startTime']
+
+        elif status == 'LIVE':
+            thisGame['scores'] = eachGame['score']['innings']
+            thisGame['currentInning'] = eachGame['score']['currentInning']
+            thisGame['homeScore'] = eachGame['score']['homeScoreTotal']
+            thisGame['awayScore'] = eachGame['score']['awayScoreTotal']
+            thisGame['homeHits'] = eachGame['score']['homeHitsTotal']
+            thisGame['awayHits'] = eachGame['score']['awayHitsTotal']
+            thisGame['homeErrors'] = eachGame['score']['homeErrorsTotal']
+            thisGame['awayErrors'] = eachGame['score']['awayErrorsTotal']
+        else:
+            thisGame['scores'] = eachGame['score']['innings']
+            thisGame['homeScore'] = eachGame['score']['homeScoreTotal']
+            thisGame['awayScore'] = eachGame['score']['awayScoreTotal']
+            thisGame['homeHits'] = eachGame['score']['homeHitsTotal']
+            thisGame['awayHits'] = eachGame['score']['awayHitsTotal']
+            thisGame['homeErrors'] = eachGame['score']['homeErrorsTotal']
+            thisGame['awayErrors'] = eachGame['score']['awayErrorsTotal']
+
+        games.append(thisGame)
+
+    return games
+
+
+
 def get_stat(all, stat):
 
     top = []
@@ -16,13 +90,18 @@ def get_stat(all, stat):
     for eachPlayer in all:
         try:
             eachPlayer['stats'][mod]
-            print(stat)
-            print(mod)
+
+            if mod == 'pitching' and float(eachPlayer['stats'][mod]['inningsPitched']) < 25.0:
+                continue
+            elif mod == 'batting' and float(eachPlayer['stats'][mod]['atBats']) < 50.0:
+                continue
+
 
             if len(top) < 5:
 
                 top.append([eachPlayer['player']['firstName'], eachPlayer['player']['lastName'], eachPlayer['stats'][mod][stat]])
 
+                print('this')
 
             else:
 
@@ -30,19 +109,20 @@ def get_stat(all, stat):
 
                     repIn = float(top[place][2])
                     repNew = float(eachPlayer['stats'][mod][stat])
+
                     if stat == 'earnedRunAvg':
                         h = repNew
                         repNew = repIn
                         repIn = h
 
-                        if repNew >= repIn:
+                    if repNew >= repIn:
 
-                            player = [eachPlayer['player']['firstName'], eachPlayer['player']['lastName'], eachPlayer['stats'][mod][stat]]
-                            top.insert(place, player)
-                            del top[-1]
-                            break
-
+                        player = [eachPlayer['player']['firstName'], eachPlayer['player']['lastName'], eachPlayer['stats'][mod][stat]]
+                        top.insert(place, player)
+                        del top[-1]
+                        break
         except:
-            print('nothing found')
+            continue
+
 
     return top
